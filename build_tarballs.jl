d16e8597416b5530a7321f75e63381fce78d8bbf
mtsch <matijacufar@gmail.com>
2019-01-11 09:13:57 +0100

    enable MacOS build

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -22,7 +22,7 @@ elif [[ $(uname) == "Darwin" ]]; then
 else
     EXT="so"
 fi
-g++ -std=c++11 -Ofast -fPIC -shared -L. -D NDEBUG -D USE_COEFFICIENTS -D ASSEMBLE_REDUCTION_MATRIX -D LIBRIPSER ripser.py/ripser/ripser.cpp -o libripser.$EXT
+$CXX -std=c++11 -Ofast -fPIC -shared -L. -D NDEBUG -D USE_COEFFICIENTS -D ASSEMBLE_REDUCTION_MATRIX -D LIBRIPSER ripser.py/ripser/ripser.cpp -o libripser.$EXT
 mkdir $prefix/lib
 mv libripser.$EXT $prefix/lib
 """
@@ -39,6 +39,7 @@ platforms = [
     Linux(:x86_64, libc=:musl),
     Linux(:aarch64, libc=:musl),
     Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    BinaryProvider.MacOS(),
     FreeBSD(:x86_64),
     Windows(:i686),
     Windows(:x86_64)
